680595556666e594f78e0f39c4e356e667457c11
Marcus Rands <mrands@hotmail.co.uk>
2018-02-18 18:15:46 +0000

    Refactored BitmapEditor run method. Extracted conditional in to provide method.

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -10,16 +10,9 @@ class BitmapEditor
   end
 
   def run(file)
-    instructions = parser.parse(file)
-    instructions.each do |instruction|
-      if instruction[0] == "I" && command_valid?(instruction)
-        create_bitmap(instruction[1], instruction[2])
-      elsif command_valid?(instruction)
-        raise bitmap_error if bitmap_does_not_exist
-        bitmap.command_interpreter(instruction)
-      else
-        raise 'Please enter a valid command.'
-      end
+    commands = parser.parse(file)
+    commands.each do |command|
+      command_router(command)
     end
   end
 
@@ -29,6 +22,17 @@ class BitmapEditor
   end
 
 private
+
+  def command_router(command)
+    if command[0] == "I" && command_valid?(command)
+      create_bitmap(*command[1..2])
+    elsif command_valid?(command)
+      raise bitmap_error if bitmap_does_not_exist
+      bitmap.command_interpreter(command)
+    else
+      raise 'Please enter a valid command.'
+    end
+  end
 
   def command_valid?(instruction)
     instruction[0] =~ /[ICLVHS]/ ? true : false
